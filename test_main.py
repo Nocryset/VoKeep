8812264ded073ef7a0a546441f2ef4768a207cb4
nocryset <nocryset@gmail.com>
2019-12-26 17:27:17 +0200

    Add editing words Add test

--- test_main.py
+++ test_main.py
@@ -1,6 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Table, create_engine, exists, and_
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, create_engine, exists, and_, func
 from sqlalchemy.orm import relationship, sessionmaker
+import os
+from time import sleep
+
+
+def clear():
+    return os.system('cls')
+
 
 #  Connection
 keys = {
@@ -25,6 +32,7 @@ engine = create_engine(
 
 # Mapping
 Base = declarative_base()
+
 
 # class User(Base):
 #     __tablename__ = 'user'
@@ -69,6 +77,10 @@ class Word(Base):
     language_id = Column(Integer, ForeignKey('language.id'))
     language = relationship("Language", back_populates="word")
 
+    def __repr__(self):
+        return "Word - '%s'" % (
+            self.value)
+
 
 class Language(Base):
     __tablename__ = 'language'
@@ -80,21 +92,6 @@ class Language(Base):
 
 
 Base.metadata.create_all(engine)
-
-
-# if not exist
-# def get_or_create(session, model, defaults=None, **kwargs):
-#     instance = session.query(model).filter_by(**kwargs).first()
-#     if instance:
-#         return instance, False
-#     else:
-#         params = dict((k, v) for k, v in kwargs.iteritems() if not isinstance(v, ClauseElement))
-#         params.update(defaults or {})
-#         instance = model(**params)
-#         session.add(instance)
-#         return instance, True
-
-# Session creation
 
 Session = sessionmaker()
 Session.configure(bind=engine)
@@ -152,28 +149,87 @@ def show_languages():
 
 
 def show_word_actions(cur_lang, lrn_lang):
-    all_words = session.query(Word).filter(Word.language == current_lang).\
-        all()
-    if len(all_words):
-        for word in all_words:
-            print(word.value, word.language.value)
-    else:
-        print("No words")
-    print("Your native is {0}\nYou learn {1}\n".format(cur_lang.value, lrn_lang.value))
+    # all_words = session.query(Word).filter(Word.language == current_lang).\
+    #     all()
+    # if len(all_words):
+    #     for word in all_words:
+    #         print(word.value, word.language.value)
+    # else:
+    #     print("No words")
+    sleep(1.5)
+    print("Your native is {0}\n"
+          "You learn {1}".format(cur_lang.value, lrn_lang.value))
     command = input("Input menu (input number):\n"
                     "1 - Add 1 word (word - translation)\n"
-                    "2 - Add words with cascade (words - translations\n"
+                    "2 - Add words with cascade (words - translations)\n"
                     "3 - Delete word (experemental)\n"
                     "4 - Show all translation\n"
+                    "5 - Start translation test\n"
+                    "6 - Edit word\n"
                     "0 - To close program\n")
+    os.system('cls')
     if command == '1':
         print(add_translation(cur_lang, lrn_lang))
+    elif command == '2':
+        add_translations(cur_lang, lrn_lang)
+    elif command == '4':
+        show_translations()
+    elif command == '5':
+        translation_test()
+    elif command == '6':
+        edit_word()
     elif command == '0':
+        exit()
         return 0
     show_word_actions(cur_lang, lrn_lang)
 
 
+def add_translations(cur_lang, lrn_lang):
+    words = list(input("Language '{0}'. Enter words with ',' as splitter:\n".format(cur_lang.value)).split(','))
+    print("Enter translation on language '{0}' below:".format(lrn_lang.value))
+
+    translations = dict()
+    translation_list = list()
+
+    for word in words:
+        kay = word
+        value = input("{0} - ".format(word))
+        translations[kay] = value
+
+        word_current = add_word(cur_lang, input_word=word)
+        word_learned = add_word(lrn_lang, input_word=translations[word])
+
+        translation_current = session.query(Translation). \
+            filter(Translation.word1 == word_current, Translation.word2 == word_learned). \
+            first()
+
+        if not translation_current:
+            created_translation = Translation(word1=word_current, word2=word_learned)
+            translation_list.append(created_translation)
+
+    print("Created next words and save in DB")
+    print(translation_list, sep="\n")
+
+    session.add_all(translation_list)
+    session.commit()
+
+    # session.info()
+    # words
+    # print(translation.values(),
+    #       translation.keys(),
+    #       translation.items(), sep="\n")
+    pass
+
+
 def show_translations():
+    try:
+        limit = int(input("Enter count results you wont to show (For Example: 10): "))
+    except ValueError:
+        limit = 10
+    translations = session.query(Translation).limit(limit)
+    for translation in translations:
+        print(translation)
+
     pass
 
 
@@ -181,8 +237,8 @@ def add_translation(cur_lang, lrn_lang):
     word_current = add_word(cur_lang)
     word_learned = add_word(lrn_lang)
     created_translation = Translation(word1=word_current, word2=word_learned)
-    translation = session.query(Translation).\
-        filter(Translation.word1_id == word_current.id, Translation.word2_id == word_learned.id).\
+    translation = session.query(Translation). \
+        filter(Translation.word1_id == word_current.id, Translation.word2_id == word_learned.id). \
         first()
     if not translation:
         session.add(created_translation)
@@ -191,8 +247,12 @@ def add_translation(cur_lang, lrn_lang):
     return translation
 
 
-def add_word(language):
-    value = input("Language {0} of next word is ".format(language.value))
+def add_word(language, input_word=None):
+    if input_word:
+        value = input_word
+        pass
+    else:
+        value = input("Language {0} of next word is ".format(language.value))
     # this value in db?
     word_in_db = session.query(Word).filter(Word.value == value, Word.language == language).first()
     if word_in_db:
@@ -205,7 +265,67 @@ def add_word(language):
     return word
 
 
-def add_words():
+def edit_word():
+    word = input('Enter word that you need edit:\n')
+    edited_word = session.query(Word).filter(Word.value == word).first()
+    if edited_word:
+        print(edited_word)
+        edited_word.value = input("Edited word:\n")
+        session.add(edited_word)
+        session.commit()
+        print("Success! Word '{0}' edited to '{1}'.".format(word, edited_word.value))
+    else:
+        print("Word '%s' not found!\n".format(word))
+    pass
+
+
+def translation_test():
+    #  DEBUG
+    # print(vocabluary)
+
+    vocabluary = session.query(Translation).filter(Word.language == current_lang).all()
+    if not vocabluary:
+        print("No words!")
+        return 0
+
+    print("1 - get mode {0} to {1}\n"
+          "2 - get mode {1} to {0}".format(current_lang.value, learn_lang.value))
+    good_words = list()
+    bad_words = list()
+
+    command = input()
+    clear()
+    if command == "1":
+        words = session.query(Word).filter(Word.language == current_lang).\
+            order_by(func.random()).limit(20).all()
+        print(words)
+    elif command == "2":
+        words = session.query(Word).filter(Word.language == learn_lang).\
+            order_by(func.random()).limit(20).all()
+        print(words)
+    else:
+        print("Bad input!")
+        return 0
+
+    for word in words:
+        value = input("{0} > ".format(word.value))
+        translation = session.query(Translation).filter(Word.value == word.value).all()
+        passed = 0
+        for tran in translation:
+            if tran.word1 == word:
+                if tran.word2.value == value:
+                    passed += 1
+            if tran.word2 == word:
+                if tran.word1.value == value:
+                    passed += 1
+        if passed:
+            good_words.append(word.value)
+            print('Good!')
+        else:
+            bad_words.append(word.value)
+            print('Bad!')
+    print("Number of Correct words:", len(good_words))
+    print("Number of Incorrect words:", len(bad_words))
     pass
 
 
@@ -214,7 +334,6 @@ current_lang = languages[0]
 learn_lang = languages[1]
 show_word_actions(cur_lang=current_lang, lrn_lang=learn_lang)
 
-
 # word1 = Word(value="get", language=current_lang)
 # word2 = Word(value="взять", language=learn_lang)
 # my_translation = Translation(word1=word1, word2=word2)
@@ -222,5 +341,5 @@ show_word_actions(cur_lang=current_lang, lrn_lang=learn_lang)
 # s = session.query(Translation).all()
 # for i in s:
 #     print(s)
-#session.add(my_translation)
+# session.add(my_translation)
 # session.commit()
